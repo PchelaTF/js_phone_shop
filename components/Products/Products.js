d5296077ec6de37e1ee7b7674f579ae3462fc4e2
PchelaTF <PchelaTF@gmail.com>
2022-08-15 00:27:32 +0300

    realized class for work with localStorage in localStorageUtil, add in components Products listener for btns and integrated localSrorageUtil

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -1,9 +1,37 @@
 class Products {
+    constructor() {
+        this.actveClass = '_active';
+        this.labelAdd = 'Add to basket';
+        this.labelRemove = 'Remove from basket';
+    }
+
+    handelSetLocalStorage(element, id) {
+        let { pushProduct, products } = localStorageUtil.putProducts(id);
+
+        if (pushProduct) {
+            element.classList.add(this.actveClass);
+            element.innerHTML = this.labelRemove;
+        } else {
+            element.classList.remove(this.actveClass);
+            element.innerHTML = this.labelAdd;
+        }
+    }
 
     render() {
+        const productsStore = localStorageUtil.getProducts();
         let catalogProducts = '';
 
         CATALOG.forEach(({ id, name, img, price }) => {
+            let activeClass = '';
+            let buttonText = '';
+
+            if (productsStore.indexOf(id) === -1) {
+                buttonText = this.labelAdd;
+            } else {
+                activeClass = ' ' + this.actveClass;
+                buttonText = this.labelRemove;
+            }
+
             catalogProducts += `
                 <li class="item">
                     <h4 class="item__name">${name}</h4>
@@ -11,7 +39,7 @@ class Products {
                         <img src="${img}" alt="phone">
                     </div>
                     <p class="item__price">Price: <span>${price}</span> $</p>
-                    <button class="item__button">Add to basket</button>
+                    <button class="item__button${activeClass}" onclick="pageProducts.handelSetLocalStorage(this, '${id}')">${buttonText}</button>
                 </li>
             `;
         });
@@ -25,5 +53,16 @@ class Products {
     }
 }
 
-const products = new Products();
-products.render();
+const pageProducts = new Products();
+pageProducts.render();
+
+
+// 1. Добавляем активный класс для кнопки
+// 2. Активный текст для кнопки
+// 3. Добавляем localStorageUtil.getProducts
+// 4. IndexOf id Проверка
+// 5. добавляем обработчик события на кнопку onclick c медотодом
+// 6. создаем метод для кнопки
+// метод вызывается через экземпляр класса
+// 7. в метод при клике передаём this
+// 8. в метод для кнопки передаем метод который будет помещать значение в локал
